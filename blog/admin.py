96c4dfa64680a1d2f10263f609229af22f5c15b9
Adebayo Yuusuf <adebayoayomide396@gmail.com>
2024-07-11 06:05:34 -0300

    Implemented the data and models and displaying them on the pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Author, Posts,Tag
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_filter=("author","tag","date")
+    list_display=("title","date","author")
+    prepopulated_fields={"slug":("title",)}
+admin.site.register(Posts,PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+
